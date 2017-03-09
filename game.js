8b876032820c6443f16304899e2348f6314177ab
Keerthana <sekarkeerthu@gmail.com>
2017-03-09 13:45:25 -0500

    Update game.js

--- game.js
+++ game.js
@@ -5,29 +5,32 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
     computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+} 
+else if (computerChoice <= 0.67) {
     computerChoice = "paper";
-} else {
+} 
+else {
     computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+} 
 
-var compare=function(choice1,choice2){
-    if(choice1=== choice2)
+console.log("Computer: " + computerChoice);
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2)
         return "The result is a tie!";
-    else if(choice1==="rock"){
-        if(choice2==="scissors")
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors")
             return "rock wins";
         else
             return "paper wins";
     }
-    else if(choice1==="paper"){
-        if(choice2==="scissors")
+    else if (choice1 === "paper") {
+        if (choice2 === "scissors")
             return "scissors wins";
         else
             return "paper wins";
     }
-    else if(choice1==="scissors"){
-        if(choice2==="paper")
+    else if (choice1 === "scissors") { 
+        if (choice2 === "paper")
             return "scissors wins";
         else
             return "rock wins";
